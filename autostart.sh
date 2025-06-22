82cd0321430aef758bdbffbb17b714bb2220f566
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-06-22 13:34:47 +0300

    Fix autostart

--- autostart.sh
+++ autostart.sh
@@ -7,15 +7,12 @@ function run {
   fi
 }
 run nm-applet
-run pamac-tray
-run variety
-run xfce4-power-manager
+run aarchup --aur --loop-time 60
 run blueberry-tray
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run volumeicon
-run xautolock -time 60 -locker "betterlockscreen -l" -detectsleep
 run xfce4-clipman
-
+run xautolock -time 60 -locker "betterlockscreen -l" -detectsleep
 
 #  disabling energy star features
 xset -dpms
